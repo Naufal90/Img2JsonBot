3cee99ba77a54302e4ee69ed0e4a352823d34b52
Naufal90 <45502745+Naufal90@users.noreply.github.com>
2025-02-01 12:21:39 +0700

    Create constants.js

---
+++ src/config/constants.js
@@ -0,0 +1,14 @@
+// src/config/constants.js
+module.exports = {
+    COLOR_TO_BLOCK: {
+        '#000000': 'black_concrete',
+        '#ffffff': 'white_concrete',
+        '#ff0000': 'red_concrete',
+        '#00ff00': 'green_concrete',
+        '#0000ff': 'blue_concrete',
+        '#ffff00': 'yellow_concrete',
+        '#ff00ff': 'magenta_concrete',
+        '#00ffff': 'cyan_concrete',
+        '#808080': 'stone',
+    },
+};
